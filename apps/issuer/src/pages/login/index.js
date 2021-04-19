2cc97993327b8d2b0e022a025df373758dd3bdcd
Giannis Papadakis <gpapadakis@admin.grnet.gr>
2021-04-19 10:32:39 +0300

    fix: fix login page put genericTemplate

--- apps/issuer/src/pages/login/index.js
+++ apps/issuer/src/pages/login/index.js
@@ -1,23 +1,14 @@
 import React, { useCallback } from "react";
 import { makeStyles } from "@material-ui/core";
-import Header, { HeaderTitle } from "@digigov/ui/app/Header";
 import PageTitle, { PageTitleHeading } from "@digigov/ui/app/PageTitle";
 import Button from "@digigov/ui/core/Button";
-import ServiceBadge from "@digigov/ui/core/ServiceBadge";
-import GovGRFooter from "@digigov/ui/govgr/Footer";
-import GovGRLogo from "@digigov/ui/govgr/Logo";
-import BasicLayout, {
-  Bottom,
-  Content,
-  Main,
-  Top,
-} from "@digigov/ui/layouts/Basic";
+import { Main } from "@digigov/ui/layouts/Basic";
 import Paragraph from "@digigov/ui/typography/Paragraph";
 import useAuth from "@digigov/auth";
+import GenericTemplate from "src/components/genericTemplate";
 
 const useStyles = makeStyles(
   {
-    top: { minHeight: "75px" },
     main: {},
     side: {},
   },
@@ -35,32 +26,18 @@ export default function Index() {
   }, [auth.config.loginURL]);
 
   return (
-    <BasicLayout>
-      <Top className={styles.top}>
-        <Header>
-          <GovGRLogo />
-          <HeaderTitle>
-            Service name
-            <ServiceBadge label="PREALPHA" />
-          </HeaderTitle>
-        </Header>
-      </Top>
-      <Content>
-        <Main className={styles.main}>
-          <PageTitle>
-            <PageTitleHeading>Login Page</PageTitleHeading>
-          </PageTitle>
-          <Paragraph>Welcome text</Paragraph>
-          {!auth.authenticated ? (
-            <Button onClick={demoLogin}>Login</Button>
-          ) : (
-            <Button onClick={() => auth.logout("/")}>Logout</Button>
-          )}
-        </Main>
-      </Content>
-      <Bottom>
-        <GovGRFooter />
-      </Bottom>
-    </BasicLayout>
+    <GenericTemplate>
+      <Main className={styles.main}>
+        <PageTitle>
+          <PageTitleHeading>Login Page</PageTitleHeading>
+        </PageTitle>
+        <Paragraph>Welcome text</Paragraph>
+        {!auth.authenticated ? (
+          <Button onClick={demoLogin}>Login</Button>
+        ) : (
+          <Button onClick={() => auth.logout("/")}>Logout</Button>
+        )}
+      </Main>
+    </GenericTemplate>
   );
 }
