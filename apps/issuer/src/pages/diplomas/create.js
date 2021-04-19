fa8d688454fc7d9e82b87b210e8ba3990834eebb
Giannis Papadakis <gpapadakis@admin.grnet.gr>
2021-04-19 10:30:15 +0300

    fix: fix diplomas page, put genericTemplate

--- apps/issuer/src/pages/diplomas/create.js
+++ apps/issuer/src/pages/diplomas/create.js
@@ -3,19 +3,13 @@ import { makeStyles } from "@material-ui/core";
 import Header, { HeaderTitle } from "@digigov/ui/app/Header";
 import PageTitle, { PageTitleHeading } from "@digigov/ui/app/PageTitle";
 import Button from "@digigov/ui/core/Button";
-import ServiceBadge from "@digigov/ui/core/ServiceBadge";
-import GovGRFooter from "@digigov/ui/govgr/Footer";
-import GovGRLogo from "@digigov/ui/govgr/Logo";
-import BasicLayout, {
-  Bottom,
-  Content,
-  Main,
-  Top,
-} from "@digigov/ui/layouts/Basic";
+import { Main } from "@digigov/ui/layouts/Basic";
 import Paragraph from "@digigov/ui/typography/Paragraph";
 import useAuth from "@digigov/auth";
 import FormBuilder, { Field } from "@digigov/form";
 import { useResourceAction } from "@digigov/ui/api";
+import GenericTemplate from "src/components/genericTemplate";
+
 export const fields = [
   {
     key: "degree",
@@ -105,7 +99,6 @@ export const fields = [
 
 const useStyles = makeStyles(
   {
-    top: { minHeight: "75px" },
     main: {},
     side: {},
   },
@@ -128,41 +121,27 @@ export default function CreateDiploma() {
   }, [formData]);
   /* debugger; */
   return (
-    <BasicLayout>
-      <Top className={styles.top}>
-        <Header>
-          <GovGRLogo />
-          <HeaderTitle>
-            Service name
-            <ServiceBadge label="PREALPHA" />
-          </HeaderTitle>
-        </Header>
-      </Top>
-      <Content>
-        <Main className={styles.main}>
-          <PageTitle>
-            <PageTitleHeading>Login Page</PageTitleHeading>
-          </PageTitle>
-          <Paragraph>Welcome text</Paragraph>
-          <FormBuilder
-            fields={fields}
-            onSubmit={(data) => {
-              setFormData(data);
-            }}
-          >
-            {fields.map((field) => (
-              <Field key={field.key} name={field.key} />
-            ))}
+    <GenericTemplate>
+      <Main className={styles.main}>
+        <PageTitle>
+          <PageTitleHeading>Create New Title</PageTitleHeading>
+        </PageTitle>
+        <Paragraph>Welcome text</Paragraph>
+        <FormBuilder
+          fields={fields}
+          onSubmit={(data) => {
+            setFormData(data);
+          }}
+        >
+          {fields.map((field) => (
+            <Field key={field.key} name={field.key} />
+          ))}
 
-            <Button type="submit" disabled={loading}>
-              Submit
-            </Button>
-          </FormBuilder>
-        </Main>
-      </Content>
-      <Bottom>
-        <GovGRFooter />
-      </Bottom>
-    </BasicLayout>
+          <Button type="submit" disabled={loading}>
+            Submit
+          </Button>
+        </FormBuilder>
+      </Main>
+    </GenericTemplate>
   );
 }
