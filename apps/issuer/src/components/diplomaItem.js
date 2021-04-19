d7b274bd0e5317e0d0b6a29fdff5dea8d88f68d7
Giannis Papadakis <gpapadakis@admin.grnet.gr>
2021-04-19 10:26:03 +0300

    feat: create diplomaItem component

---
+++ apps/issuer/src/components/diplomaItem.js
@@ -0,0 +1,48 @@
+import Typography from "@material-ui/core/Typography";
+import Link from "next/link";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+
+export default function DiplomaItem(props) {
+  const url = `/diplomas/${props.id}`;
+  return (
+    <Link href={url}>
+      <ListItem button alignItems="flex-start">
+        <ListItemText
+          secondary={
+            <Typography variant="body2" color="textPrimary">
+              Όνομα:{" "}
+              <>
+                <strong>{props.userName}</strong> |{" "}
+              </>
+              Τίτλος Σπουδών:{" "}
+              <>
+                <strong>{props.degree}</strong> |{" "}
+              </>
+              Είδος τίτλου Σπουδών:{" "}
+              <>
+                <strong>{props.typeOfDegree}</strong> |{" "}
+              </>
+              Τμήμα/Σχολή:{" "}
+              <>
+                <strong>{props.school}</strong> |{" "}
+              </>
+              Ίδρυμα:{" "}
+              <>
+                <strong>{props.institution}</strong> |{" "}
+              </>
+              Έτος:{" "}
+              <>
+                <strong>{props.year}</strong> |{" "}
+              </>
+              Status:{" "}
+              <>
+                <strong>{props.status}</strong> |{" "}
+              </>
+            </Typography>
+          }
+        />
+      </ListItem>
+    </Link>
+  );
+}
