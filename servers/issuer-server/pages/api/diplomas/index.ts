b97352a4c6a7dbe9a7cd148701a2d5892600fe20
Giannis Papadakis <gpapadakis@admin.grnet.gr>
2021-04-22 12:07:42 +0300

    chore: clean up unnecessary code

--- servers/issuer-server/pages/api/diplomas/index.ts
+++ servers/issuer-server/pages/api/diplomas/index.ts
@@ -1,46 +1,3 @@
-/* export const diplomas = [
-  {
-    id: 1,
-    degree: "Μηχανικος",
-    typeOfDegree: "Πολιτικός Μηχανικός",
-    school: "ΜΠΔ",
-    institution: "TUC",
-    status: "έγκριση",
-    userName: "Νίκος Γκάλης",
-    year: "2000",
-  },
-  {
-    id: 2,
-    degree: "Μηχανικος",
-    typeOfDegree: "Πληροφορική",
-    school: "ΜΗΠΕΡ",
-    institution: "TUC",
-    status: "σε εκκρεμότητα",
-    userName: "Παναγίωτης Φασούλας",
-    year: "2004",
-  },
-  {
-    id: 3,
-    degree: "Μηχανικος",
-    typeOfDegree: "Πληροφορική",
-    school: "ΗΜΜΥ",
-    institution: "ΑΣΟΕΕ",
-    status: "έγκριση",
-    userName: "Παναγιώτης Γιαννάηκης",
-    year: "2003",
-  },
-  {
-    id: 4,
-    degree: "Μηχανικος",
-    typeOfDegree: "Πολιτικός Μηχανικός",
-    school: "ΗΜΜΥ",
-    institution: "ΑΣΟΕΕ",
-    status: "σε εκκρεμότητα",
-    userName: "Λευτέρης Μπακατσιάς",
-    year: "2002",
-  },
-]; */
-
 export const valuePool = {
   degree: ["engineer", "architec"],
   typeOfDegree: ["programmer", "architec"],
@@ -72,30 +29,6 @@ export const diplomas = new Array(100).fill({}).map((item, index) => {
     year: valuePool.year[Math.floor(Math.random() * valuePool.year.length)],
   };
 });
-
-/* export default async function handler(req, res) {
-  let response = null;
-  if (req.method === "POST") {
-    // Process a POST request
-    response = res.status(200).json({ ok: true });
-  } else {
-    const limit = parseInt(req.query.limit) || 10;
-    const offset = parseInt(req.query.offset) || 0;
-    const requestedDiplomas = diplomas
-      .filter((item) => {
-        return Object.values(item).join(" ").includes(req.query.search);
-      })
-      .slice(offset, offset + limit);
-    response = res.send({
-      data: requestedDiplomas,
-      meta: {
-        total: diplomas.length / limit,
-        count: diplomas.length,
-      },
-    });
-  }
-  return response;
-} */
 
 export default async function handler(req, res) {
   let response = null;
