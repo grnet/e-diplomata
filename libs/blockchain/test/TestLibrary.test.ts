12a332a506caafa18b5bcb494762372f296ca0bd
athiklei <athiklei@admin.grnet.gr>
2021-04-26 14:09:34 +0300

    feat(issuer): update test file

--- libs/blockchain/test/TestLibrary.test.ts
+++ libs/blockchain/test/TestLibrary.test.ts
@@ -6,18 +6,20 @@ import {
   publishAck,
   publishFail
 } from '../src/index';
-
-import { ethers } from 'ethers';
 import { eventsABI } from '../src/eventsABI';
+/*Uncomment one of the following depending on the provider*/
+//gia ganache topika
+import { provider, account, address } from '../src/Ganache';
+//gia INFURA
+//import { provider, myaccount, myaddress } from '../src/Infura';
 
 const abiDecoder = require('abi-decoder');
-const provider = new ethers.providers.JsonRpcProvider('http://localhost:8545');
 const dummyTxHash = '0x63d6709a4d465d1223c9247cd4fd6c54c29f9e209f627d307c1bbdaea3b9a776';
 const dummyHash = '0xca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb';
 
 let contractAddress = '';
   beforeAll( async() => {
-    const result = await deployContract();
+    const result = await deployContract({accountIssuer: account});
     contractAddress = result.contractAddress;
     const requestABI = eventsABI;
     /* use abiDecoder to decode the logs and find the parameters of the emited event
@@ -31,13 +33,15 @@ describe('testing functions in index.ts ', () => {
     const transcactionHashAward = await publishAward({
       hashOfAwardFirstPart: dummyHash,
       hashOfAwardSecondPart: dummyHash, 
-      contractAddressUsedByIssuer: contractAddress
+      contractAddressUsedByIssuer: contractAddress,
+      provider: provider,
+      accountIssuer: account,
+      addressIssuer: address
     });
     /*search the Blockchain to find the transaction receipt.
       the search is done using the transactionHash of a transaction
     */
     const receipt = await provider.getTransactionReceipt(transcactionHashAward.transactionHash);
-    
     const decodedLogs = abiDecoder.decodeLogs(receipt.logs);
     expect(decodedLogs[0].events[0].value).toBe(dummyHash);
   });
@@ -50,14 +54,15 @@ describe('testing functions in index.ts ', () => {
       c: dummyHash,
       c2: dummyHash,
       nirenc: dummyHash,
-      ev: dummyHash
+      ev: dummyHash,
+      provider: provider,
+      accountIssuer: account,
+      addressIssuer: address
     });
     /*search the Blockchain to find the transaction receipt.
       the search is done using the transactionHash of a transaction
     */
     const receipt = await provider.getTransactionReceipt(transactionHashProof.transactionHash);
-    
-
     const decodedLogs = abiDecoder.decodeLogs(receipt.logs);
     expect(decodedLogs[0].events[0].value).toBe(dummyTxHash);
   });
@@ -69,13 +74,15 @@ describe('testing functions in index.ts ', () => {
       VerifKeyPart1: dummyHash,
       VerifKeyPart2: dummyHash,
       VerifKeyPart3: dummyHash,
-      VerifKeyPart4: dummyHash
+      VerifKeyPart4: dummyHash,
+      provider: provider,
+      accountHolder: account,
+      addressHolder: address
     });
     /*search the Blockchain to find the transaction receipt.
       the search is done using the transactionHash of a transaction
     */
     const receipt = await provider.getTransactionReceipt(transactionHashRequest.transactionHash);
-    
     const decodedLogs = abiDecoder.decodeLogs(receipt.logs);
     expect(decodedLogs[0].events[0].value).toBe(dummyTxHash);
   });
@@ -84,13 +91,15 @@ describe('testing functions in index.ts ', () => {
     const transactionHashAck = await publishAck({
       contractAddressUsedByIssuer: contractAddress,
       sPrf: dummyTxHash,
-      eI: dummyHash
+      eI: dummyHash,
+      provider: provider,
+      accountVerifier: account,
+      addressVerifier: address
     });
     /*search the Blockchain to find the transaction receipt.
       the search is done using the transactionHash of a transaction
     */
-    const receipt = await provider.getTransactionReceipt(transactionHashAck.transactionHash);
-    
+    const receipt = await provider.getTransactionReceipt(transactionHashAck.transactionHash);    
     const decodedLogs = abiDecoder.decodeLogs(receipt.logs);
     expect(decodedLogs[0].events[0].value).toBe(dummyTxHash);
   });
@@ -98,13 +107,15 @@ describe('testing functions in index.ts ', () => {
   it('should publish a fail and then check that the correct data where published', async () => {
     const transactionHashFail = await publishFail({
       contractAddressUsedByIssuer: contractAddress,
-      sPrf: dummyTxHash
+      sPrf: dummyTxHash,
+      provider: provider,
+      accountVerifier: account,
+      addressVerifier: address
     });
     /*search the Blockchain to find the transaction receipt.
       the search is done using the transactionHash of a transaction
     */
-    const receipt = await provider.getTransactionReceipt(transactionHashFail.transactionHash);
-    
+    const receipt = await provider.getTransactionReceipt(transactionHashFail.transactionHash);    
     const decodedLogs = abiDecoder.decodeLogs(receipt.logs);
     expect(decodedLogs[0].events[0].value).toBe(dummyTxHash);
   });
