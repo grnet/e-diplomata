ba80bcf62f46810984def1d40468d96ef90cf9c9
Giannis Papadakis <gpapadakis@admin.grnet.gr>
2021-05-11 14:25:31 +0300

    fix(issuer-server): fix db  working properly on upadate

--- servers/issuer-server/src/lib/db.js
+++ servers/issuer-server/src/lib/db.js
@@ -1,14 +1,11 @@
 const Datastore = require("nedb");
 
-const database = new Datastore("diplomas.db");
-database.loadDatabase(function (error) {
-  if (error) {
-    console.log(
-      "FATAL: local database could not be loaded. Caused by: " + error
-    );
-  } else {
-    console.log("INFO: local database loaded successfully.");
-  }
-});
+const database = new Datastore({ filename: "diplomas.db", autoload: true });
 
-export default database;
+const db = () => {
+  return database;
+}
+
+database.persistence.compactDatafile();
+
+export default db;
