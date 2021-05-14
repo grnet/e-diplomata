e8abbf0b04483a49da0c57cb433fadc280103229
gpapadakis <gpapadakis@admin.grnet.gr>
2021-05-14 14:32:23 +0300

    feat(holder-server): create titles api for holder

---
+++ servers/holder-server/src/pages/api/titles/index.js
@@ -0,0 +1,47 @@
+import database from "holder-server/lib/db";
+
+
+export default async function handler(req, res) {
+  const db =database();
+  let response = null;
+  if (req.method === "POST") {
+    // Process a POST request
+    response = res.status(200).json({ ok: true });
+  } else {
+    let counter = 0;
+    const limits = parseInt(req.query.limit) || 10;
+    const offsets = parseInt(req.query.offset) || 0;
+    const skip = (offsets - 1) * limits;
+    const searchValue = req.query.search || "";
+    const { limit, offset, search, ...queries } = req.query;
+    db.count(
+      {
+        ...queries,
+        $where: function () {
+          return Object.values(this).join(" ").includes(searchValue);
+        },
+      },
+      function (err, count) {
+        counter = count;
+      }
+    );
+    db.find({
+      ...queries,
+      $where: function () {
+        return Object.values(this).join(" ").includes(searchValue);
+      },
+    })
+      .skip(skip)
+      .limit(limits)
+      .exec(function (err, data) {
+        response = res.send({
+          data: data,
+          meta: {
+            total: counter / limits,
+            count: counter,
+          },
+        });
+      });
+  }
+  return response;
+}
