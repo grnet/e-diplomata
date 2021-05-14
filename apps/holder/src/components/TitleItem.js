d960f555134b34b9516f63af4e4fcbd9d6c9aba0
gpapadakis <gpapadakis@admin.grnet.gr>
2021-05-14 14:26:47 +0300

    feat(holder): create title item for holder list

---
+++ apps/holder/src/components/TitleItem.js
@@ -0,0 +1,40 @@
+import Typography from "@material-ui/core/Typography";
+import Link from "next/link";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+
+export default function TitleItem(props) {
+  const url = `/titles/${props._id}`;
+  return (
+    <Link href={url}>
+      <ListItem button alignItems="flex-start">
+        <ListItemText
+          secondary={
+            <Typography variant="body2" color="textPrimary">
+              Τίτλος Σπουδών:{" "}
+              <>
+                <strong>{props.degree}</strong> |{" "}
+              </>
+              Είδος τίτλου Σπουδών:{" "}
+              <>
+                <strong>{props.typeOfDegree}</strong> |{" "}
+              </>
+            Τμήμα/Σχολή:{" "}
+              <>
+                <strong>{props.school}</strong> |{" "}
+              </>
+              Ίδρυμα:{" "}
+              <>
+                <strong>{props.institution}</strong> |{" "}
+              </>
+              Status:{" "}
+              <>
+                <strong>{props.status}</strong> |{" "}
+              </>
+            </Typography>
+          }
+        />
+      </ListItem>
+    </Link>
+  );
+}
