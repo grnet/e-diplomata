9f2e5f377f9619af4bf11fb35cf2906f1467205c
gpapadakis <gpapadakis@admin.grnet.gr>
2021-05-24 13:58:09 +0300

    feat(verifier-server): create  api call to find title

---
+++ servers/verifier-server/src/pages/api/titles/[id].ts
@@ -0,0 +1,8 @@
+import db from "verifier-server/lib/db";
+
+export default async function handler(req, res) {
+  db().findOne({ _id: req.query.id }, function (err, data) {
+    return res.status(200).json(data);
+  });
+}
+
