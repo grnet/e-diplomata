f52e7cfb95219b34a6c1cd6cfef9c9f24c82a464
gpapadakis <gpapadakis@admin.grnet.gr>
2021-05-24 13:48:22 +0300

    fix(verifier): remove css form VerifierLayout component

--- apps/verifier/src/components/VerifierLayout.js
+++ apps/verifier/src/components/VerifierLayout.js
@@ -2,21 +2,12 @@ import BasicLayout, { Bottom, Top, Content } from "@digigov/ui/layouts/Basic";
 import GovGRFooter from "@digigov/ui/govgr/Footer";
 import GovGRLogo from "@digigov/ui/govgr/Logo";
 import Header, { HeaderTitle } from "@digigov/ui/app/Header";
-import { makeStyles } from "@material-ui/core";
 import ServiceBadge from "@digigov/ui/core/ServiceBadge";
 
-const useStyles = makeStyles(
-  {
-    top: { minHeight: "75px" },
-  },
-  { name: "MuiSite" }
-);
-
 export default function HolderLayout({ children }) {
-  const styles = useStyles();
   return (
     <BasicLayout>
-      <Top className={styles.top}>
+      <Top>
         <Header>
           <GovGRLogo />
           <HeaderTitle>
