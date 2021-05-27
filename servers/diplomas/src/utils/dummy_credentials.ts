26dcc97407901af9c9a518fc7d3411232d8027e8
gpapadakis <gpapadakis@admin.grnet.gr>
2021-05-27 15:57:33 +0300

    feat(diplomas): create login dummy credentials data

---
+++ servers/diplomas/src/utils/dummy_credentials.ts
@@ -0,0 +1,72 @@
+const holderCredentials = [
+    {
+        firstName: 'Mario',
+        lastName: 'Menexe',
+        fatherName: 'Niko',
+        email: 'mario@menexe.com',
+        password: '1234567'
+    },
+    {
+        firstName: 'Aggeliki',
+        lastName: 'Antoniou',
+        fatherName: 'Billy',
+        email: 'aggeliki@antoniou.com',
+        password: '1234567',
+    },
+    {
+        firstName: 'Nikos',
+        lastName: 'Likos',
+        fatherName: 'Zonni',
+        email: 'nikos@likos.com',
+        password: '1234567',
+    },
+    {
+        firstName: 'Antonia',
+        lastName: 'Vazelou',
+        fatherName: 'Nikos',
+        email: 'antonia@vazelou.com',
+        password: '1234567',
+    },
+    {
+        firstName: 'Olga',
+        lastName: 'Feretou',
+        fatherName: 'Dimos',
+        email: 'olga@feretou.com',
+        password: '1234567',
+    },
+];
+
+const verifierCredentials = [
+    {
+        title: 'My Company',
+        email: 'root@mycompany.com',
+        password: '1234567'
+    },
+    {
+        title: 'Happy Company',
+        email: 'root@happycompany.com',
+        password: '1234567',
+    },
+    {
+        title: 'State Desk',
+        email: 'root@statedesk.gr',
+        password: '1234567',
+    },
+
+];
+
+const issuerCredentials = [
+    {
+        title: 'University of Athens',
+        email: 'root@uoa.gr',
+        password: '1234567',
+    },
+    {
+        title: 'University of Crete',
+        email: 'root@uoc.gr',
+        password: '1234567',
+    },
+
+];
+
+export { holderCredentials, verifierCredentials, issuerCredentials};
