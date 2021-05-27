4f2cceb0316b812194f531b40b919293933a5159
gpapadakis <gpapadakis@admin.grnet.gr>
2021-05-27 15:47:31 +0300

    fix(issuer): put Select dropdown menu,  fetch dummy credentials

--- apps/issuer/src/pages/login/index.js
+++ apps/issuer/src/pages/login/index.js
@@ -1,27 +1,35 @@
-import React, {useCallback} from "react";
-import {makeStyles} from "@material-ui/core";
-import PageTitle, {PageTitleHeading} from "@digigov/ui/app/PageTitle";
+import React, { useCallback } from "react";
+import { makeStyles } from "@material-ui/core";
+import PageTitle, { PageTitleHeading } from "@digigov/ui/app/PageTitle";
 import Button from "@digigov/ui/core/Button";
-import {Main} from "@digigov/ui/layouts/Basic";
+import { Main } from "@digigov/ui/layouts/Basic";
 import Paragraph from "@digigov/ui/typography/Paragraph";
 import useAuth from "@digigov/auth";
 import IssuerLayout from "issuer/components/IssuerLayout";
-import FormBuilder, {Field} from '@digigov/form';
+import FormBuilder, { Field } from '@digigov/form';
+import { useResource } from "@digigov/ui/api";
+import Select from '@material-ui/core/NativeSelect';
 
-const useStyles = makeStyles(
-  {
-    main: {},
-    side: {},
+const useStyles = makeStyles((theme) => ({
+  main: {},
+  side: {},
+  formControl: {
+    margin: theme.spacing(2),
+    minWidth: 120,
+    marginLeft: theme.spacing(0),
   },
-  {name: "MuiSite"}
-);
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}/* ,{ name: "MuiSite" } */));
 
 export default function Index() {
   const styles = useStyles();
   const auth = useAuth();
+  const { data } = useResource('issuer/dummy_credentials');
   const demoLogin = useCallback(async (user) => {
     window.setTimeout(() => {
-      window.localStorage.setItem("login-next", "/diplomas");
+      /* window.localStorage.setItem("login-next", "/diplomas"); */
       // window.location.href = auth.config.loginURL + "?username=test";
     }, 1);
     const response = await fetch('/api/issuer/login', {
@@ -30,7 +38,7 @@ export default function Index() {
       headers: {
         'Content-Type': 'application/json'
       }
-    }).then(res=>res.json());
+    }).then(res => res.json());
     auth.authenticate(response.token)
   }, [auth.config.loginURL]);
 
@@ -40,35 +48,51 @@ export default function Index() {
         <PageTitle>
           <PageTitleHeading>Login Page</PageTitleHeading>
         </PageTitle>
+
         {!auth.authenticated ? (
-          <FormBuilder fields={[
-            {
-              key: 'password',
-              label: {
-                primary: 'Password'
+          <>
+            <FormBuilder fields={[
+              {
+                key: 'password',
+                label: {
+                  primary: 'Password'
+                },
+                type: 'string',
+                extra: {
+                  type: 'password'
+                }
               },
-              type: 'string',
-              extra: {
-                type: 'password'
-              }
-            },
-            {
-              key: 'email',
-              label: {
-                primary: 'Email'
+              {
+                key: 'email',
+                label: {
+                  primary: 'Email'
+                },
+                type: 'string'
               },
-              type: 'string'
-            },
-          ]}
-            onSubmit={(data) => {
-              console.log(data);
-              demoLogin(data);
-            }}
-          >
-            <Field name={'email'} />
-            <Field name={'password'} />
-            <Button type='submit'>Login</Button>
-          </FormBuilder>
+            ]}
+              onSubmit={(data) => {
+                console.log(data);
+                demoLogin(data);
+              }}
+            >
+              <Field name={'email'} />
+              <Field name={'password'} />
+              <Button type='submit'>Login</Button>
+            </FormBuilder>
+            {data &&
+              <Select
+                className={styles.formControl}
+                onChange={(e) => {
+                  const user = data[e.target.value];
+                  demoLogin(user);
+                }}
+              >
+                {data &&
+                  data.map((issuer, index) => (
+                    <option key={index} value={index}>{issuer.title}</option>
+                  ))}
+              </Select>}
+          </>
         ) : (
           <Button onClick={() => auth.logout("/")}>Logout</Button>
         )}
