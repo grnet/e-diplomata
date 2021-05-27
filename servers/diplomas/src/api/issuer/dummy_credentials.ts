9fdcd62eedac04429317f2919331ee1407ed0009
gpapadakis <gpapadakis@admin.grnet.gr>
2021-05-27 15:53:26 +0300

    feat(diplomas): create api service for issuer dummy_data

---
+++ servers/diplomas/src/api/issuer/dummy_credentials.ts
@@ -0,0 +1,9 @@
+import { issuerCredentials } from '@diplomas/core/utils/dummy_credentials';
+
+export default {
+    get:[async function (req: any, res: any) {
+        console.log(req)
+        res.json(issuerCredentials);
+    }]
+}
+
