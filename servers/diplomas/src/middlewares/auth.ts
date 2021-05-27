81f8ba9de06151eb53ba773ab8623ff34e6fecaf
gpapadakis <gpapadakis@admin.grnet.gr>
2021-05-27 15:56:26 +0300

    fix(diplomas): fix auth

--- servers/diplomas/src/middlewares/auth.ts
+++ servers/diplomas/src/middlewares/auth.ts
@@ -1,7 +1,8 @@
 import jwt from "jsonwebtoken";
 
 
-export default function authFactory(Model:any) {
+export default function authFactory(Model: any) {
+  console.log(Model.modelName);
   return function jwtMiddleware(req: any, res: any, next: any) {
     const token = req.header("Authorization").split('Token ')[1];
     if (!token) return res.status(401).json({ message: "Auth Error" });
@@ -9,12 +10,7 @@ export default function authFactory(Model:any) {
     try {
       const decoded = jwt.verify(token, "randomString") as any;
       req.user = decoded.user;
-      req.userType = decoded.type;
-      if(Model && req.userType === Model.modelName){
-        next();
-      }else{
-        res.status(500).send({ message: "Invalid user type" });
-      }
+      next();
     } catch (e) {
       console.error(e);
       res.status(500).send({ message: "Invalid Token" });
