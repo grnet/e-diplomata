0f124b9c1b996df956f1ded9b8997378e7daf74b
gpapadakis <gpapadakis@admin.grnet.gr>
2021-06-03 16:53:30 +0300

    fix(index): use StartPage component in template

--- apps/issuer/src/pages/index.tsx
+++ apps/issuer/src/pages/index.tsx
@@ -1,21 +1,15 @@
 import React from 'react';
-import  { Main } from '@digigov/ui/layouts/Basic';
-import Paragraph from '@digigov/ui/typography/Paragraph';
-import PageTitle, {PageTitleHeading} from '@digigov/ui/app/PageTitle';
-import CallToActionButton from '@digigov/ui/core/Button/CallToAction';
-import IssuerLayout from 'src/components/IssuerLayout';
+import StartPage from "@diplomas/design-system/StartPage";
 
 
 export default function Index() {
   return (
-    <IssuerLayout>
-      <Main >
-          <PageTitle>
-            <PageTitleHeading>Ediplomas issuer service</PageTitleHeading>
-          </PageTitle>
-          <Paragraph>You can issue diploma in the blockchain</Paragraph>
-          <CallToActionButton href="/login?next=/diplomas">Enter here</CallToActionButton>
-        </Main>
-    </IssuerLayout>
+    <>
+      <StartPage
+        title = "Ediplomas issuer service"
+        text = "You can issue diploma in the blockchain"
+        href ="/login?next=/diplomas"
+      />
+    </>
   );
 }
