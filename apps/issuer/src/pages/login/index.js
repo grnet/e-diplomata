ef02765506390ec8dc02b0e79fe315d577b7308a
gpapadakis <gpapadakis@admin.grnet.gr>
2021-06-07 15:18:07 +0300

    fix(issuer): put type login for issuer data

--- apps/issuer/src/pages/login/index.js
+++ apps/issuer/src/pages/login/index.js
@@ -1,16 +1,17 @@
 import React from "react";
-import Layout from "@diplomas/design-system/Layout";
-import Login from "@diplomas/design-system/Login";;
+import Login from "@diplomas/design-system/Login";
 import { useResource } from "@digigov/ui/api";
 
 export default function Index() {
   const { data } = useResource('issuer/dummy_credentials');
   const url = '/api/issuer/login';
+  const type = "Issuer";
   return (
     <>
       <Login
         data={data}
         url={url}
+        type={type}
       />
     </>
 
