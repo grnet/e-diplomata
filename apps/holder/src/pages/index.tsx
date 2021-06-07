8174889bb464670b0aa095e8497701ecf0ecc2ae
gpapadakis <gpapadakis@admin.grnet.gr>
2021-06-07 15:06:08 +0300

    fix(holder): fix holder start page with global StartPage component

--- apps/holder/src/pages/index.tsx
+++ apps/holder/src/pages/index.tsx
@@ -1,22 +1,15 @@
 import React from 'react';
-import { Main } from '@digigov/ui/layouts/Basic';
-import Paragraph from '@digigov/ui/typography/Paragraph';
-import PageTitle, { PageTitleHeading } from '@digigov/ui/app/PageTitle';
-import CallToActionButton from '@digigov/ui/core/Button/CallToAction';
-import Layout from "@diplomas/design-system/Layout";
+import StartPage from "@diplomas/design-system/StartPage";
+
 
 export default function Index() {
   return (
-    
-    <Layout>
-      <Main >
-        <PageTitle>
-          <PageTitleHeading>Titles holder service</PageTitleHeading>
-        </PageTitle>
-        <Paragraph>You can issue diploma in the blockchain</Paragraph>
-        <CallToActionButton href="/login?next=/titles">Enter here</CallToActionButton>
-      </Main>
-    </Layout>
-  )
-    
+    <>
+      <StartPage
+        title="Titles holder service"
+        text="You can issue diploma in the blockchain"
+        href="/login?next=/titles"
+      />
+    </>
+  );
 }
