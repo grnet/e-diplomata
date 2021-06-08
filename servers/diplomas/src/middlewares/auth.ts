6d61c14b405f02af116b97de4b47429a9b602329
Vasilis Kefallinos <vkefallinos@gmail.com>
2021-06-08 15:14:45 +0300

    feat(server): add user object in request context

--- servers/diplomas/src/middlewares/auth.ts
+++ servers/diplomas/src/middlewares/auth.ts
@@ -3,13 +3,14 @@ import jwt from "jsonwebtoken";
 
 export default function authFactory(Model: any) {
   console.log(Model.modelName);
-  return function jwtMiddleware(req: any, res: any, next: any) {
+  return async function jwtMiddleware(req: any, res: any, next: any) {
     const token = req.header("Authorization").split('Token ')[1];
     if (!token) return res.status(401).json({ message: "Auth Error" });
 
     try {
       const decoded = jwt.verify(token, "randomString") as any;
-      req.user = decoded.user;
+      const user = await Model.findById(decoded.user.id)
+      req.user = user;
       next();
     } catch (e) {
       console.error(e);
