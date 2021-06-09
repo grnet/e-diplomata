6097b71d78e596743c72842e563d685940b3ef3c
Vasilis Kefallinos <vkefallinos@gmail.com>
2021-06-09 19:09:26 +0300

    feat(ledger): add ledger class implementation

--- libs/ledger/src/index.ts
+++ libs/ledger/src/index.ts
@@ -6,18 +6,43 @@ export * from './providers/Ganache';
 export * from './providers/Infura';
 export * from './publish';
 export * from './types';
-
-import {getTransaction} from './getTransaction';
-import {publish} from './publish';
-import {LedgerInterface} from '@diplomas/protocol'
+import generateWallet, { GenerateWalletInput } from './generateWallet'
+import { getTransaction, getTransactionSync } from './getTransaction';
+import { publish } from './publish';
+import { LedgerConstructorInterface, LedgerInterface } from '@diplomas/protocol'
 
 class Ledger implements LedgerInterface {
-  getTransaction(...args) {
-    return getTransaction(...args)
-  }  
-  publish(...args) {
-    return publish(...args)
-  }  
+  config: LedgerConstructorInterface;
+
+  constructor(props: LedgerConstructorInterface) {
+    this.config = props
+  }
+  static generateWallet(input: GenerateWalletInput){
+    return generateWallet(input)
+  }
+  getTransaction(
+    hash: string,
+    minConfirmations?: number
+  ) {
+    return getTransaction({
+      ...this.config,
+      hash,
+      minConfirmations
+    })
+  }
+  getTransactionSync(
+    hash: string,
+    minConfirmations?: number
+  ) {
+    return getTransactionSync({
+      ...this.config,
+      hash,
+      minConfirmations
+    })
+  }
+  publish(signature: string) {
+    return publish({ ...this.config, signature })
+  }
 }
 
 export default Ledger
