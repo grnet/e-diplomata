929e863b780439d5c8e8f464867bf1b43bbef172
Vasilis Kefallinos <vkefallinos@gmail.com>
2021-06-09 19:10:21 +0300

    feat(storage): clean up storage implementation

--- libs/storage/src/index.ts
+++ libs/storage/src/index.ts
@@ -1,72 +1,24 @@
 import {StorageInterface} from '@diplomas/protocol'
 
-type Status =  'pending' | 'confirmed' |'fail'; 
-
-interface IssuerDocument {
-  file: any;
-  holder: string;
-  issuer: string;
-  s_awd: string;
-  t_awd: string;
-  c: any;
-  r: string;
-  status: Status;
-}
-
-interface HolderDocument {
-  file: any;
-  holder: string;
-  issuer: string;
-  s_awd: string;
-  t_awd: string;
-  c: any;
-}
-
-interface HolderRequest {
-  s_awd: string;
-  s_req: string;
-  t_req: string;
-  status: Status;
-  verifier: string;
-  holder: string;
-  issuer: string;
-}
-
-interface ProofRequest {
-  s_awd: string;
-  t_req: string;
-  s_prf: string;
-  t_prf: string;
-  proof: any;
-  status: Status;
-  verifier: string;
-  holder: string;
-  issuer: string;
-}
-
-interface Acknowledgment {
-  t_prf: string;
-  proof: any;
-  issuer: string;
-  t_ack: string;
-  s_ack: string;
-  status: Status;
-  result: Status;
-
-}
 
 export class MongooseStorage implements StorageInterface {
   mongoose: any;
   constructor(mongoose){
     this.mongoose = mongoose
   }
-  getBy(type, query){
-    return this.mongoose.model(type).findOne(query)
+  async getBy(type, query){
+    const result = await this.mongoose.model(type).findOne(query).exec()
+    return result.toJSON()
   }
-  create(type, data){
-    return this.mongoose.model(type).create(data)
+  async create(type, data){
+    const result = await this.mongoose.model(type).create(data)
+    return result.toJSON()
   }
-  update(type, id, data){
-    return this.mongoose.model(type).update({id},data)
+  async update(type, id, data){
+    const result = await this.mongoose.model(type).updateOne({_id:id},data).exec()
+    return result
   }
 }
+
+
+export default MongooseStorage
