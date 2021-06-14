b4a8b5f968d25e7a22c793474eb32fe2c83632b8
gpapadakis <gpapadakis@admin.grnet.gr>
2021-06-14 12:38:10 +0300

    fix(issuer): modify diplomas component with prensentation data

--- apps/issuer/src/pages/diplomas/index.js
+++ apps/issuer/src/pages/diplomas/index.js
@@ -1,21 +1,37 @@
 import React from "react";
 import Documents from "@diplomas/design-system/Documents";
-import DiplomaItem from "issuer/components/diplomaItem";
 
 export default function Diplomas() {
   const types = ['Bachelor', 'Master', 'Doctorate'];
   const departments = ['Main', 'Science'];
   const dataFilters = [{ title: "Είδος τίτλου σπουδών", filterData: types, filterTypeData: "type" }, { title: "Ίδρυμα/Σχολή", filterData: departments, filterTypeData: "department" }];
-  const url ="issuer/documents";
+  const url = "issuer/documents";
   const title = "Τίτλοι Σπουδών";
+  const presentation = {
+    url: (row) => `/diplomas/${row._id}`,
+    fields: [
+      {
+        label: 'Τιτλος σπουδων',
+        key: 'title'
+      },
+      {
+        label: 'Είδος τίτλου Σπουδών',
+        key: 'type'
+      },
+      {
+        label: 'Τμήμα/Σχολή',
+        key: 'department'
+      }
+    ]
+  }
   return (
     <>
       <Documents
         dataFilters={dataFilters}
         url={url}
         title={title}
+        presentation={presentation}
       >
-        <DiplomaItem />
       </Documents>
     </>
   );
